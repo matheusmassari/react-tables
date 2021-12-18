77ae62365ddee530c06a4cb7c94b3070ac160c59
Matheus Massari <massari@Matheuss-MacBook-Air.local>
2021-12-18 11:36:02 -0300

    tabel basica com css w3schools

---
+++ src/components/BasicTable.js
@@ -0,0 +1,102 @@
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./columns";
+import styled from "styled-components";
+
+export const BasicTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  });
+
+  return (
+    <Container>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => {
+            return (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => {
+                  return (
+                    <th {...column.getHeaderProps()}>
+                      {column.render("Header")}
+                    </th>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map((footerGroup) => {
+            return (
+              <tr {...footerGroup.getFooterGroupProps()}>
+                {footerGroup.headers.map((column) => (
+                  <td {...column.getFooterProps}>{column.render("Footer")}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tfoot>
+      </table>
+    </Container>
+  );
+};
+
+export const Container = styled.div`
+  margin-top: 4rem;
+
+  table {
+    font-family: Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    margin: 0 auto;
+    width: 80%;
+  }
+
+  table td,
+  table th {
+    border: 1px solid #ddd;
+    padding: 8px;
+  }
+
+  table tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+
+  table tr:hover {
+    background-color: #ddd;
+  }
+
+  table th, tfoot td {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: center;
+    background-color: #04aa6d;
+    color: white;
+  }
+`;
